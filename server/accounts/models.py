e8dc3f55dfc72e7ca23d924a15d066f82c5d38f0
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2020-10-23 18:50:05 +0600

    Custom user model added

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    department = models.CharField(max_length=5)
+    university = models.CharField(max_length=100)
+    reg_no = models.CharField(max_length=10, blank=True, null=True)
