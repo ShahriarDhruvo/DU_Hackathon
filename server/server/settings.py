39779e2bfe282cfccffa85cc9cf1ea31c16be084
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2021-01-14 21:36:58 +0600

    making full-stack functional

--- server/server/settings.py
+++ server/server/settings.py
@@ -102,6 +102,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': 'du_hackathon',
         # 'USER': 'postgres',
+        # #'PASSWORD': '2369',
         # 'PASSWORD': 'emonsust',
         # 'HOST': 'localhost',
         # 'PORT': '',
@@ -192,11 +193,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get('APP_EMAIL_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('APP_EMAIL_PASS')
-
-EMAIL_HOST_USER = "whyblankistaken@gmail.com"
-EMAIL_HOST_PASSWORD = "llrnecuoqscezmua"
+EMAIL_HOST_USER = os.environ.get('APP_EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('APP_EMAIL_PASS')
 
 OLD_PASSWORD_FIELD_ENABLED = True
 
